5093088373be03a16c03c81da81b4b7e3a643825
CarlosUziel <s1781011@ed.ac.uk>
2018-06-11 11:37:41 +0100

    Trying network with 3 channels.

--- deepmedic/versions/DM_V1/run.sh
+++ deepmedic/versions/DM_V1/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-../../deepMedicRun -dev cuda -newModel ./configFiles/model/modelConfig.cfg -train ./configFiles/train/trainConfig.cfg &>run_output
+../../deepMedicRun -dev cuda -newModel ./configFiles/model/modelConfig.cfg -train ./configFiles/train/trainConfig.cfg #&>run_output
