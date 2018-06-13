4f9359928029eac2763e973c1cedd7cd39958a40
CarlosUziel <s1781011@ed.ac.uk>
2018-06-13 10:25:30 +0100

    Preparing for transfer-learning.

---
+++ deepmedic/versions/DM_V1/run_transfer.sh
@@ -0,0 +1,2 @@
+../../deepMedicRun -dev cuda -train ./configFiles_transfer/train/trainConfig.cfg -resetOptimizer -model ~/DISS/milestones/DM_V1/output/cnnModels/trainSession_DM_V1/*final*.save #&>run_output
+
