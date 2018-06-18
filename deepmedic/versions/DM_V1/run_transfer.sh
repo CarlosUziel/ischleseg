94190d7197871ef50d04aa2fb3d5740176489b40
CarlosUziel <s1781011@ed.ac.uk>
2018-06-18 11:59:26 +0100

    Updated versions structure.

--- deepmedic/versions/DM_V1/run_transfer.sh
+++
@@ -1,2 +0,0 @@
-../../deepMedicRun -dev cuda -train ./configFiles_transfer/train/trainConfig.cfg -resetOptimizer -model ~/DISS/milestones/DM_V1/output_DM_V1/cnnModels/trainSession_DM_V1/*final*.save #&>run_output
-
--- deepmedic/versions/DM_V2/run_transfer.sh
+++
@@ -1,2 +0,0 @@
-../../deepMedicRun -dev cuda -train ./configFiles_transfer/train/trainConfig.cfg -resetOptimizer -model ~/DISS/milestones/DM_V2/output_DM_V2/cnnModels/trainSession_DM_V2/*final*.save #&>run_output
-
