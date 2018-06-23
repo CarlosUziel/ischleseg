a4baeef02a5d9fc1ec50729a786f8795e15ccf4f
CarlosUziel <cauzpema@gmail.com>
2018-06-23 11:45:11 +0100

    Updated run scripts

--- deepmedic/versions/replicate_base.sh
+++ deepmedic/versions/replicate_base.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
+echo DM_V0_{0..9} | xargs -n 1 cp DM_V0_base/run.sh
+echo DM_V1_{0..9} | xargs -n 1 cp DM_V1_base/run.sh
 echo DM_V0_transfer_{0..9} | xargs -n 1 cp DM_V0_transfer_base/run_transfer.sh
 echo DM_V1_transfer_{0..9} | xargs -n 1 cp DM_V1_transfer_base/run_transfer.sh
