29d467c834637df8de6d631e0b712608458fbfe0
CarlosUziel <cauzpema@gmail.com>
2018-06-25 17:23:52 +0100

    Added scripts to run full models. Postprocessing of those still pending

---
+++ deepmedic/versions/DM_V3/run_test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+../../deepMedicRun -dev cuda -test ./configFiles/test/testConfig_0.cfg -model ./output/cnnModels/trainSession/*final*.save
+
+../../deepMedicRun -dev cuda -test ./configFiles/test/testConfig_1.cfg -model ./output/cnnModels/trainSession/*final*.save
