f4607445e688a5b548abf330ad519846196f82dd
CarlosUziel <cauzpema@gmail.com>
2018-06-26 20:31:49 +0100

    Update files

--- deepmedic/versions/DM_V3_transfer/run_transfer.sh
+++ deepmedic/versions/DM_V3_transfer/run_transfer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm -rf output
-../../deepMedicRun -dev cuda -train ./configFiles/train/trainConfig.cfg -resetOptimizer -model ../DM_V3/output/cnnModels/trainSession/*final*.save
+../../deepMedicRun -dev cuda -train ./configFiles/train/trainConfig.cfg -resetOptimizer -model ../DM_V2/output/cnnModels/trainSession/*final*.save
 
