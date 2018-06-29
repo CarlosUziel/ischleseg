6238755982e58856ae167d1ce7fe7f51726f5535
CarlosUziel <s1781011@ed.ac.uk>
2018-06-29 13:20:10 +0100

    Remove previous output before execution.

--- deepmedic/versions/run_all_V1.sh
+++ deepmedic/versions/run_all_V1.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+rm -rf ./DM_V1_0/output
 ../deepMedicRun -dev cuda -newModel ./DM_V1_0/configFiles/model/modelConfig.cfg -train ./DM_V1_0/configFiles/train/trainConfig.cfg
 #../deepMedicRun -dev cuda -train ./DM_V1_transfer_0/configFiles/train/trainConfig.cfg -resetOptimizer -model ./DM_V1_0/output/cnnModels/trainSession/*final*.save
 
+rm -rf ./DM_V1_1/output
 ../deepMedicRun -dev cuda -newModel ./DM_V1_1/configFiles/model/modelConfig.cfg -train ./DM_V1_1/configFiles/train/trainConfig.cfg
 #../deepMedicRun -dev cuda -train ./DM_V1_transfer_1/configFiles/train/trainConfig.cfg -resetOptimizer -model ./DM_V1_1/output/cnnModels/trainSession/*final*.save
 
+rm -rf ./DM_V1_2/output
 ../deepMedicRun -dev cuda -newModel ./DM_V1_2/configFiles/model/modelConfig.cfg -train ./DM_V1_2/configFiles/train/trainConfig.cfg
 #../deepMedicRun -dev cuda -train ./DM_V1_transfer_2/configFiles/train/trainConfig.cfg -resetOptimizer -model ./DM_V1_2/output/cnnModels/trainSession/*final*.save
 
+rm -rf ./DM_V1_3/output
 ../deepMedicRun -dev cuda -newModel ./DM_V1_3/configFiles/model/modelConfig.cfg -train ./DM_V1_3/configFiles/train/trainConfig.cfg
 #../deepMedicRun -dev cuda -train ./DM_V1_transfer_3/configFiles/train/trainConfig.cfg -resetOptimizer -model ./DM_V1_3/output/cnnModels/trainSession/*final*.save
 
+rm -rf ./DM_V1_4/output
 ../deepMedicRun -dev cuda -newModel ./DM_V1_4/configFiles/model/modelConfig.cfg -train ./DM_V1_4/configFiles/train/trainConfig.cfg
 #../deepMedicRun -dev cuda -train ./DM_V1_transfer_4/configFiles/train/trainConfig.cfg -resetOptimizer -model ./DM_V1_4/output/cnnModels/trainSession/*final*.save
