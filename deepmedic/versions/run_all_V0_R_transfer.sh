f2ca1a784abe5ee79862d9efbcadb9305280693d
CarlosUziel <s1781011@ed.ac.uk>
2018-07-08 18:41:01 +0100

    Added execution script for V0_R_transfer

---
+++ deepmedic/versions/run_all_V0_R_transfer.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+rm -rf ./DM_V0_R_transfer_0/output
+../deepMedicRun -dev cuda -train ./DM_V0_R_transfer_0/configFiles/train/trainConfig.cfg -resetOptimizer -model ./DM_V0_R_0/output/cnnModels/trainSession/*final*.save
+../deepMedicRun -dev cuda -model ./DM_V0_R_transfer_0/output/cnnModels/trainSession/*final*.save -test ./DM_V0_R_transfer_0/configFiles/test/testConfig.cfg 
+
+
+rm -rf ./DM_V0_R_transfer_1/output
+../deepMedicRun -dev cuda -train ./DM_V0_R_transfer_1/configFiles/train/trainConfig.cfg -resetOptimizer -model ./DM_V0_R_1/output/cnnModels/trainSession/*final*.save
+../deepMedicRun -dev cuda -model ./DM_V0_R_transfer_1/output/cnnModels/trainSession/*final*.save -test ./DM_V0_R_transfer_1/configFiles/test/testConfig.cfg 
+
+
+rm -rf ./DM_V0_R_transfer_2/output
+../deepMedicRun -dev cuda -train ./DM_V0_R_transfer_2/configFiles/train/trainConfig.cfg -resetOptimizer -model ./DM_V0_R_2/output/cnnModels/trainSession/*final*.save
+../deepMedicRun -dev cuda -model ./DM_V0_R_transfer_2/output/cnnModels/trainSession/*final*.save -test ./DM_V0_R_transfer_2/configFiles/test/testConfig.cfg 
+
+
+rm -rf ./DM_V0_R_transfer_3/output
+../deepMedicRun -dev cuda -train ./DM_V0_R_transfer_3/configFiles/train/trainConfig.cfg -resetOptimizer -model ./DM_V0_R_3/output/cnnModels/trainSession/*final*.save
+../deepMedicRun -dev cuda -model ./DM_V0_R_transfer_3/output/cnnModels/trainSession/*final*.save -test ./DM_V0_R_transfer_3/configFiles/test/testConfig.cfg 
+
+
+rm -rf ./DM_V0_R_transfer_4/output
+../deepMedicRun -dev cuda -train ./DM_V0_R_transfer_4/configFiles/train/trainConfig.cfg -resetOptimizer -model ./DM_V0_R_4/output/cnnModels/trainSession/*final*.save
+../deepMedicRun -dev cuda -model ./DM_V0_R_transfer_4/output/cnnModels/trainSession/*final*.save -test ./DM_V0_R_transfer_4/configFiles/test/testConfig.cfg
