20105cb9df04e9561e02c58b621d42354a45b9a1
CarlosUziel <cauzpema@gmail.com>
2018-07-15 16:47:53 +0100

    Updated some execution scripts.

--- deepmedic/versions/run_on_titan.sh
+++ deepmedic/versions/run_on_titan.sh
@@ -2,3 +2,5 @@
 
 CUDA_VISIBLE_DEVICES=1 ./run_all_V1.sh
 CUDA_VISIBLE_DEVICES=1 ./run_all_V1_transfer.sh
+CUDA_VISIBLE_DEVICES=1 ./run_all_V1_R.sh
+CUDA_VISIBLE_DEVICES=1 ./run_all_V1_R_transfer.sh
