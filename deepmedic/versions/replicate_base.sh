f64ff9f2706babefd3a1eb6a3e2df4c594e31413
CarlosUziel <cauzpema@gmail.com>
2018-07-19 22:02:47 +0100

    Updated learning rate scheduler to exponential

--- deepmedic/versions/replicate_base.sh
+++ deepmedic/versions/replicate_base.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
-echo DM_V0_{0..4} | xargs -n 1 cp DM_V0_base/run_train.sh
-echo DM_V0_{0..4} | xargs -n 1 cp DM_V0_base/run_test.sh
-echo DM_V0_{0..4}/configFiles/train/trainConfig.cfg | xargs -n 1 cp DM_V0_base/configFiles/train/trainConfig.cfg
-echo DM_V0_{0..4}/configFiles/test/testConfig.cfg | xargs -n 1 cp DM_V0_base/configFiles/test/testConfig.cfg
+echo DM_V0_{0..4} | xargs -n 1 cp -r DM_V0_base/*
+echo DM_V0_transfer_{0..4} | xargs -n 1 cp -r DM_V0_transfer_base/*
 
-echo DM_V1_{0..4} | xargs -n 1 cp DM_V1_base/run_train.sh
-echo DM_V1_{0..4} | xargs -n 1 cp DM_V1_base/run_test.sh
-echo DM_V1_{0..4}/configFiles/train/trainConfig.cfg | xargs -n 1 cp DM_V1_base/configFiles/train/trainConfig.cfg
-echo DM_V1_{0..4}/configFiles/test/testConfig.cfg | xargs -n 1 cp DM_V1_base/configFiles/test/testConfig.cfg
+echo DM_V0_R_{0..4} | xargs -n 1 cp -r DM_V0_R_base/*
+echo DM_V0_R_transfer_{0..4} | xargs -n 1 cp -r DM_V0_R_transfer_base/*
 
-echo DM_V0_transfer_{0..4} | xargs -n 1 cp DM_V0_transfer_base/run_transfer.sh
-echo DM_V0_transfer_{0..4} | xargs -n 1 cp DM_V0_transfer_base/run_test.sh
-echo DM_V0_transfer_{0..4}/configFiles/train/trainConfig.cfg | xargs -n 1 cp DM_V0_transfer_base/configFiles/train/trainConfig.cfg
-echo DM_V0_transfer_{0..4}/configFiles/test/testConfig.cfg | xargs -n 1 cp DM_V0_transfer_base/configFiles/test/testConfig.cfg
+echo DM_V1_{0..4} | xargs -n 1 cp -r DM_V1_base/*
+echo DM_V1_transfer_{0..4} | xargs -n 1 cp -r DM_V1_transfer_base/*
 
-echo DM_V1_transfer_{0..4} | xargs -n 1 cp DM_V1_transfer_base/run_transfer.sh
-echo DM_V1_transfer_{0..4} | xargs -n 1 cp DM_V1_transfer_base/run_test.sh
-echo DM_V1_transfer_{0..4}/configFiles/train/trainConfig.cfg | xargs -n 1 cp DM_V1_transfer_base/configFiles/train/trainConfig.cfg
-echo DM_V1_transfer_{0..4}/configFiles/test/testConfig.cfg | xargs -n 1 cp DM_V1_transfer_base/configFiles/test/testConfig.cfg
+echo DM_V1_R_{0..4} | xargs -n 1 cp -r DM_V1_R_base/*
+echo DM_V1_R_transfer_{0..4} | xargs -n 1 cp -r DM_V1_R_transfer_base/*
 
+echo DM_V2_{0..4} | xargs -n 1 cp -r DM_V2_base/*
+echo DM_V2_R_{0..4} | xargs -n 1 cp -r DM_V2_R_base/*
