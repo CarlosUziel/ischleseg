6c0bcd1268cda1e8f1a44ed136579baf789377d2
CarlosUziel <cauzpema@gmail.com>
2018-07-20 12:56:00 +0100

    .

---
+++ env_setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+conda create -n diss_tf python=2.7 anaconda -y
+echo ". /home/uziel/anaconda2/etc/profile.d/conda.sh" >> ~/.bashrc
+source activate diss_tf #conda activate diss_tf
+conda install matplotlib -y
+conda install jupyterlab -y
+conda install git -y
+
+pip install argparse
+pip install msgpack
+pip install theano
+cd deepmedic
+pip install .
+pip install nilearn
+pip install nipype
+pip install scikit-learn
+pip install scikit-image
+pip install scipy
