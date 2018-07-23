985d86284c6b1462e505f6e7b8b420257e0b557d
CarlosUziel <cauzpema@gmail.com>
2018-07-24 00:16:51 +0100

    Fixed V2 cfg and execution script. Froze conv layers and reduced initial LR to 0.0005.

--- deepmedic/versions/run_all_V2.sh
+++ deepmedic/versions/run_all_V2.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 rm -rf ./DM_V2_0/output
-../deepMedicRun -dev cuda -newModel ./DM_V2_0/configFiles/model/modelConfig.cfg -train ./DM_V2_0/configFiles/train/trainConfig.cfg
+../deepMedicRun -dev cuda -train ./DM_V2_0/configFiles/train/trainConfig.cfg -resetOptimizer -model ./DM_V1_0/output/cnnModels/trainSession/*final*.save
 ../deepMedicRun -dev cuda -model ./DM_V2_0/output/cnnModels/trainSession/*final*.save -test ./DM_V2_0/configFiles/test/testConfig.cfg 
 ../deepMedicRun -dev cuda -model ./DM_V2_0/output/cnnModels/trainSession/*final*.save -test ./DM_V2_0/configFiles/validation/valConfig.cfg 
 
 rm -rf ./DM_V2_1/output
-../deepMedicRun -dev cuda -newModel ./DM_V2_1/configFiles/model/modelConfig.cfg -train ./DM_V2_1/configFiles/train/trainConfig.cfg
+../deepMedicRun -dev cuda -train ./DM_V2_1/configFiles/train/trainConfig.cfg -resetOptimizer -model ./DM_V1_1/output/cnnModels/trainSession/*final*.save
 ../deepMedicRun -dev cuda -model ./DM_V2_1/output/cnnModels/trainSession/*final*.save -test ./DM_V2_1/configFiles/test/testConfig.cfg 
 ../deepMedicRun -dev cuda -model ./DM_V2_1/output/cnnModels/trainSession/*final*.save -test ./DM_V2_1/configFiles/validation/valConfig.cfg 
 
 rm -rf ./DM_V2_2/output
-../deepMedicRun -dev cuda -newModel ./DM_V2_2/configFiles/model/modelConfig.cfg -train ./DM_V2_2/configFiles/train/trainConfig.cfg
+../deepMedicRun -dev cuda -train ./DM_V2_2/configFiles/train/trainConfig.cfg -resetOptimizer -model ./DM_V1_2/output/cnnModels/trainSession/*final*.save
 ../deepMedicRun -dev cuda -model ./DM_V2_2/output/cnnModels/trainSession/*final*.save -test ./DM_V2_2/configFiles/test/testConfig.cfg 
 ../deepMedicRun -dev cuda -model ./DM_V2_2/output/cnnModels/trainSession/*final*.save -test ./DM_V2_2/configFiles/validation/valConfig.cfg 
 
 rm -rf ./DM_V2_3/output
-../deepMedicRun -dev cuda -newModel ./DM_V2_3/configFiles/model/modelConfig.cfg -train ./DM_V2_3/configFiles/train/trainConfig.cfg
+../deepMedicRun -dev cuda -train ./DM_V2_3/configFiles/train/trainConfig.cfg -resetOptimizer -model ./DM_V1_3/output/cnnModels/trainSession/*final*.save
 ../deepMedicRun -dev cuda -model ./DM_V2_3/output/cnnModels/trainSession/*final*.save -test ./DM_V2_3/configFiles/test/testConfig.cfg 
 ../deepMedicRun -dev cuda -model ./DM_V2_3/output/cnnModels/trainSession/*final*.save -test ./DM_V2_3/configFiles/validation/valConfig.cfg 
 
 rm -rf ./DM_V2_4/output
-../deepMedicRun -dev cuda -newModel ./DM_V2_4/configFiles/model/modelConfig.cfg -train ./DM_V2_4/configFiles/train/trainConfig.cfg
+../deepMedicRun -dev cuda -train ./DM_V2_4/configFiles/train/trainConfig.cfg -resetOptimizer -model ./DM_V1_4/output/cnnModels/trainSession/*final*.save
 ../deepMedicRun -dev cuda -model ./DM_V2_4/output/cnnModels/trainSession/*final*.save -test ./DM_V2_4/configFiles/test/testConfig.cfg
 ../deepMedicRun -dev cuda -model ./DM_V2_4/output/cnnModels/trainSession/*final*.save -test ./DM_V2_4/configFiles/validation/valConfig.cfg 
